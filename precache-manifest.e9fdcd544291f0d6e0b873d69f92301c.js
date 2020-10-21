99e8c4989e89623f05dcca617387a88547a8227c
Sofiia Irfan <sofiirfan1206@gmail.com>
2020-10-21 20:30:52 +0300

    Updates

---
+++ precache-manifest.e9fdcd544291f0d6e0b873d69f92301c.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "31f3f834685b94a164bc17c7e49febfa",
+    "url": "/goit-react-hw-02-feedback/index.html"
+  },
+  {
+    "revision": "e5f29752625f3107bf24",
+    "url": "/goit-react-hw-02-feedback/static/css/2.3d72dcb8.chunk.css"
+  },
+  {
+    "revision": "4008da90b5412da0fe73",
+    "url": "/goit-react-hw-02-feedback/static/css/main.ca998dc7.chunk.css"
+  },
+  {
+    "revision": "e5f29752625f3107bf24",
+    "url": "/goit-react-hw-02-feedback/static/js/2.db484708.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/goit-react-hw-02-feedback/static/js/2.db484708.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4008da90b5412da0fe73",
+    "url": "/goit-react-hw-02-feedback/static/js/main.b3b0a752.chunk.js"
+  },
+  {
+    "revision": "cb08e2ec47267fd4ee9f",
+    "url": "/goit-react-hw-02-feedback/static/js/runtime-main.9cb7e66e.js"
+  }
+]);
